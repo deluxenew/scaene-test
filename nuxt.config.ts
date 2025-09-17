c5c3f605922b4b5f79245e154b8f27500344dd6b
Харчиков Александр <57594543+deluxenew@users.noreply.github.com>
2025-09-17 13:12:34 +0300

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,5 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: ['@nuxt/eslint', '@nuxt/image', '@nuxt/ui'],
- baseURL: process.env.NODE_ENV === 'production' ? '/scene-test/' : '/'
+  baseURL:  '/scene-test/'
 })
